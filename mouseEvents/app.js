0090e36329f9bc78beaccc6f2b523c339c4b86a1
skynewell1 <skyfordnewell@gmail.com>
2021-06-10 13:12:38 -0600

    push

--- mouseEvents/app.js
+++ mouseEvents/app.js
@@ -2,10 +2,6 @@
 //  to attach a "mouseover" and "mouseout" event to a h3 element.
 //The h3 should change size when mouseovered
 
-{/* <h3 id="mouse">Quick Trivia</h3> */}
-//  <!-- add a trivia question -->
- 
-//  <div onmouseover="mOver(this)" onmouseout="mOut(this)"> Capital of CO</
 
 const changeSize = document.getElementById('mouse');
 document.addEventListener('mouseover', goOver, true)
@@ -13,9 +9,10 @@ document.addEventListener('mouseout', goOut, true);
 
 function goOver() {
     document.getElementById('mouse').style.fontSize  = "125%";
+    alert ("Trivia question here?");
 }
 function goOut() {
-    document.getElementById('mouse').style.fontSize  = "100%";
+    document.getElementById('mouse').style.fontSize  = "10%";
 }
 
 //Part II
